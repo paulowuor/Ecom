1bfd70a06553f9bc319d700330415454665f2b7a
ONGIDI E GEOFREY <61456675+OngidiGeofrey@users.noreply.github.com>
2023-04-30 01:01:57 +0300

    Fix : Connection to db

--- Approved.php
+++ Approved.php
@@ -9,8 +9,7 @@ if (!$conn) {
 
 // Get the order ID and status from the AJAX request
 $orderId = $_POST['order_id'];
-$status = $_POST['status'];
-print_r($_POST);
+$status = $_POST[
 //get email address
 //$email_address=get_farmer_email_address($orderId);
 
@@ -27,107 +26,107 @@ if (!$result) {
 }
 
 
-function get_farmer_email_address($order_id=){
+// function get_farmer_email_address($order_id=){
 
-  //fetch username given order_id
+//   //fetch username given order_id
 
-  $query="SELECT username FROM order WHERE order_id=$orderId";
-  $result = mysqli_query($conn, $query);
+//   $query="SELECT username FROM order WHERE order_id=$orderId";
+//   $result = mysqli_query($conn, $query);
 
-  if($result){
+//   if($result){
 
-    $row = mysqli_fetch_assoc($result);
+//     $row = mysqli_fetch_assoc($result);
 
-    $username=$row["username"];
+//     $username=$row["username"];
 
-    //fetch email address from farmers table using username
+//     //fetch email address from farmers table using username
 
 
-    $email_query="SELECT email FROM farmers WHERE username=$username";
-    $email_result = mysqli_query($conn, $email_query);
+//     $email_query="SELECT email FROM farmers WHERE username=$username";
+//     $email_result = mysqli_query($conn, $email_query);
   
-    if($email_result){
+//     if($email_result){
   
-      $row = mysqli_fetch_assoc($email_result);
+//       $row = mysqli_fetch_assoc($email_result);
   
-      $email_address=$row["email"];
+//       $email_address=$row["email"];
   
-      //fetch email address from farmers table using username
-    }
+//       //fetch email address from farmers table using username
+//     }
 
 
-  }
+//   }
 
-  return $email_address;
-
-
-}
-
-function send_email($email_address='',$message=''){
+//   return $email_address;
 
 
-  //  initialize post fields
-                  $post_fieds = json_encode(array(
+// }
+
+// function send_email($email_address='',$message=''){
+
+
+//   //  initialize post fields
+//                   $post_fieds = json_encode(array(
                       
-                          "From"=> "geofrey.ongidi@digitalvision.co.ke",
-                          "To"=> $email_address,
-                          "Subject"=> "Hello from Postmark",
-                          "HtmlBody"=> $message,
-                          "MessageStream"=> "notifications"
+//                           "From"=> "geofrey.ongidi@digitalvision.co.ke",
+//                           "To"=> $email_address,
+//                           "Subject"=> "Hello from Postmark",
+//                           "HtmlBody"=> $message,
+//                           "MessageStream"=> "notifications"
                       
-                  ));
+//                   ));
   
-                  // if get token
-                  if($token = get_authorization_token()){
-                      $url = "https://api.postmarkapp.com/email";
-                      $curl = curl_init();
-                      curl_setopt_array($curl, array(
-                      CURLOPT_URL => $url,
-                      CURLOPT_RETURNTRANSFER => true,
-                      CURLOPT_ENCODING => "",
-                      CURLOPT_MAXREDIRS => 10,
-                      CURLOPT_TIMEOUT => 0,
-                      CURLOPT_FOLLOWLOCATION => true,
-                      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                      CURLOPT_CUSTOMREQUEST => "POST",
-                      CURLOPT_POSTFIELDS =>$post_fieds,
-                      CURLOPT_HTTPHEADER => array(
-                          "Authorization: Bearer".$token,
-                          "Content-Type: application/json",
-                          "Accept: application/json",
-                          "X-Postmark-Server-Token: b1371069-335f-431b-8f45-88c22d7f1c47"
-                      ),
-                      ));
-                      $response = curl_exec($curl);
-                      $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-                      $err = curl_error($curl);
-                      curl_close($curl);
-                      if ($err) {
+//                   // if get token
+//                   if($token = get_authorization_token()){
+//                       $url = "https://api.postmarkapp.com/email";
+//                       $curl = curl_init();
+//                       curl_setopt_array($curl, array(
+//                       CURLOPT_URL => $url,
+//                       CURLOPT_RETURNTRANSFER => true,
+//                       CURLOPT_ENCODING => "",
+//                       CURLOPT_MAXREDIRS => 10,
+//                       CURLOPT_TIMEOUT => 0,
+//                       CURLOPT_FOLLOWLOCATION => true,
+//                       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//                       CURLOPT_CUSTOMREQUEST => "POST",
+//                       CURLOPT_POSTFIELDS =>$post_fieds,
+//                       CURLOPT_HTTPHEADER => array(
+//                           "Authorization: Bearer".$token,
+//                           "Content-Type: application/json",
+//                           "Accept: application/json",
+//                           "X-Postmark-Server-Token: b1371069-335f-431b-8f45-88c22d7f1c47"
+//                       ),
+//                       ));
+//                       $response = curl_exec($curl);
+//                       $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//                       $err = curl_error($curl);
+//                       curl_close($curl);
+//                       if ($err) {
                           
-                          return FALSE;
-                      } else {                    
-                          if($response){
-                              if($file = json_decode($response)){ 
+//                           return FALSE;
+//                       } else {                    
+//                           if($response){
+//                               if($file = json_decode($response)){ 
   
-                                  //store data in db
+//                                   //store data in db
                                   
-                                  print_r($file);
+//                                   print_r($file);
                                  
-                              }else{
-                                  return FALSE;
-                              }
-                              //print_r($file);die;
-                          }else{
-                              $error = $err?:'';
-                              $code = $httpcode?:'';
-                              return FALSE;
-                          }
-                      }
-                  }else{
-                      return FALSE;
-                  }      
-  }
+//                               }else{
+//                                   return FALSE;
+//                               }
+//                               //print_r($file);die;
+//                           }else{
+//                               $error = $err?:'';
+//                               $code = $httpcode?:'';
+//                               return FALSE;
+//                           }
+//                       }
+//                   }else{
+//                       return FALSE;
+//                   }      
+//   }
   
-// Close the database connection
+// // Close the database connection
 mysqli_close($conn);
 ?>
